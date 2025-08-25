fbbe5531f212f4c18885c989fbedf12915c00464
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-08-25 16:33:58 +1000

    Update backend.tf

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,4 +1,8 @@
-resource_group_name = "rg-terraform-state"
-storage_account_name = "mtfstatestorage"
-container_name = "tfstate"
-key = "resume.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "mtfstatestorage"
+    container_name       = "tfstate"
+    key                  = "resume.tfstate"
+  }
+}
